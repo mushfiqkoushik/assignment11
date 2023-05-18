9c1a0e4018b9cc6e4a54bf2d9108d50aef2aba02
Mushfiqur Rahman Kaoushik <mushfiqkoushik@gmail.com>
2023-05-18 19:44:17 +0600

    firebase coffign

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjiHw4rYMoUzjMvm3b5Md-X8COwv6cFQs",
+  authDomain: "toy-store-a5a8e.firebaseapp.com",
+  projectId: "toy-store-a5a8e",
+  storageBucket: "toy-store-a5a8e.appspot.com",
+  messagingSenderId: "967242546730",
+  appId: "1:967242546730:web:b9b9fc02d1b7a1298a8d53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
